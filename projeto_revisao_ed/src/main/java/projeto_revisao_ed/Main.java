688d38317863711b1d940e64fad470644e457eec
MAYARA LUPETI TURBIANI <mayara.lturbiani@senacsp.edu.br>
2025-02-21 22:10:08 -0300

    aula 3 - metodos novos como remover e aumentar capacidade

--- projeto_revisao_ed/src/main/java/projeto_revisao_ed/Main.java
+++ projeto_revisao_ed/src/main/java/projeto_revisao_ed/Main.java
@@ -5,16 +5,53 @@ public class Main {
     public static void main(String[] args) throws Exception{
         Vetor vetor = new Vetor(5);
 
+        /*
         vetor.adiciona("teste 1");
         vetor.adiciona("teste 2");
-        vetor.adiciona("teste 3");
+        //vetor.adiciona("teste 3");
         //vetor.adiciona("teste 4");
         //vetor.adiciona("teste 5");
         //vetor.adiciona("teste 6");
+        
 
         System.out.println("A quantidade de informações no vetor é: " + vetor.tamanho());
         System.out.println("Dados do vetor: " + vetor.toString());
         System.out.println("Informação na posição 1: " + vetor.busca(1));
         System.out.println("Buscar o endereço da informação Elemento 1: " + vetor.buscaPorElemento("teste 2"));
+        
+        System.out.println("\n\n\n\n");
+
+        vetor.adicionaInicio(0, "teste 0");
+        System.out.println("A quantidade de informações no vetor é " + vetor.tamanho);
+        System.out.println("Dados do vetor: " + vetor.toString());
+        System.out.println("Informações na posição 1: " + vetor.busca(1));
+
+        System.out.println("\n\n\n\n");
+        */
+
+        vetor.adiciona("Elemento 0");
+        vetor.adiciona("Elemento 1");
+        vetor.adiciona("Elemento 2");
+        vetor.adiciona("Elemento 3");
+        vetor.adiciona("Elemento 4");
+        vetor.adiciona("Elemento 5");
+        vetor.adiciona("Elemento 6");
+        vetor.adiciona("Elemento 7");
+        
+
+        System.out.println("A quantidade de informações no vetor é: " + vetor.tamanho());
+        System.out.println("Dados de vetor: " + vetor.toString());
+
+        System.out.println("\n\n");
+        vetor.remove(3);
+
+        System.out.println("A quantidade de informações no vetor é: " + vetor.tamanho());
+        System.out.println("Dados de vetor: " + vetor.toString());
+
+        System.out.println("\n\n");
+        vetor.removeByElement("Elemento 5");
+
+        System.out.println("A quantidade de informações no vetor é: " + vetor.tamanho());
+        System.out.println("Dados de vetor: " + vetor.toString());
     }
 }
--- projeto_revisao_ed/src/main/java/projeto_revisao_ed/Vetor.java
+++ projeto_revisao_ed/src/main/java/projeto_revisao_ed/Vetor.java
@@ -12,11 +12,44 @@ public class Vetor {
     }
 
     public void adiciona(String elemento) throws Exception{
-        if(this.tamanho < this.elementos.length){
-            this.elementos[this.tamanho] = elemento;
-            this.tamanho++;
+        this.aumentaCapacidade();
+        this.elementos[this.tamanho] =elemento;
+        this.tamanho ++;
+    }
+
+    public boolean adicionaInicio(int posicao, String elemento) throws Exception{
+        this.aumentaCapacidade();
+        if (posicao >= 0 && posicao < tamanho) {
+            for (int i = this.tamanho - 1; i >= posicao; i--) {
+                this.elementos[i + 1] = this.elementos[i];
+            }
+            this.elementos[posicao] = elemento;
+            this.tamanho ++;
         } else {
-            throw new Exception("O vetor já está cheio, não é possível adicionar novos elementos");
+            throw new Exception("Posição inválida");
+        }
+        return true;
+    }
+
+    public void aumentaCapacidade(){
+        if (this.tamanho == this.elementos.length) {
+            String[] elementosNovos = new String[this.elementos.length * 2];
+            for (int i = 0; i < elementos.length; i++) {
+                elementosNovos[i] = this.elementos[i];
+            }
+            this.elementos = elementosNovos;
+        }
+    }
+
+    public void remove(int posicao) throws Exception{
+        if (posicao >= 0 && posicao < tamanho) {
+            for (int i = posicao; i < this.tamanho - 1 ; i++) {
+                this.elementos[i] = this.elementos[i+1];
+            }
+            this.elementos[tamanho] = null;
+            this.tamanho --;
+        } else {
+            throw new Exception("Posição inválida");
         }
     }
 
@@ -58,6 +91,16 @@ public class Vetor {
             }
         }
 
+        return -1;
+    }
+
+    public int removeByElement(String elemento) throws Exception{
+        for (int i = 0; i < tamanho; i++) {
+            if (elementos[i].equalsIgnoreCase(elemento)) {
+                this.remove(i);
+                return 1;
+            }
+        }
         return -1;
     }
 }
